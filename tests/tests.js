5a2a9ef941e6a305f5aae41be30e6e21f39d1568
Frederick Jansen <frederick.jansen@gmail.com>
2015-11-12 00:32:32 -0500

    [SKCP-20] Added dummy tests for cordova-plugin-test-framework

---
+++ tests/tests.js
@@ -0,0 +1,15 @@
+exports.defineAutoTests = function() {
+
+  describe('awesome tests', function() {
+    it('do something sync', function() {
+      expect(1).toBe(1);
+    });
+
+    it('do something async', function(done) {
+      setTimeout(function() {
+        expect(2).toBe(2);
+        done();
+      }, 100);
+    });
+  });
+};
