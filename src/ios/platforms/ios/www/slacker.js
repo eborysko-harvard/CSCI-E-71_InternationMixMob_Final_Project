3a4069e3a5ad07975e0f322a77b4f2086bea42eb
DBugMode <manojshenoy@manojshenoysmbp.fios-router.home>
2015-11-14 11:44:39 -0500

    [SKCP - 22] Add framework for testing iOS code. Issue - Build fails when code coverage is enabled.

--- src/ios/platforms/ios/www/slacker.js
+++ src/ios/platforms/ios/www/slacker.js
@@ -1,12 +1,9 @@
 
 
 var pluginName = "immSlackClient";
-var initialized = false;
 
 var Slacker = function () {};
 Slacker.authenticate = function() {
-    alert(pluginName);
-    initialized = true;
     cordova.exec(function callback(data){
                  ''
                  },function errorHandler(err){
@@ -15,8 +12,6 @@ Slacker.authenticate = function() {
 }
 
 Slacker.presence = function(userid) {
-    alert(pluginName);
-    initialized = true;
     cordova.exec(function callback(data){
                  ''
                  },function errorHandler(err){
@@ -27,5 +22,5 @@ Slacker.presence = function(userid) {
 
 function errorHandlerFunction(err)
 {
-    confirm(err);
+    alert(err);
 }
