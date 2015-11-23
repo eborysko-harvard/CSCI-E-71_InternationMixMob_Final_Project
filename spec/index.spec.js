13a7b523f01c7f14a09e61d9cba41e22133fdfad
Frederick Jansen <frederick.jansen@gmail.com>
2015-11-22 19:38:00 -0500

    [SKCP-91] Added missing spec tests

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -20,13 +20,40 @@ describe('cordova-plugin-slacker', function () {
   });
 
   describe('Slacker', function () {
-    it("should exist", function () {
+    it('should exist', function () {
       expect(Slacker).toBeDefined();
       expect(typeof Slacker == 'object').toBe(true);
+      expect(typeof Slacker.init == 'function').toBe(true);
+      expect(typeof Slacker.postMessage == 'function').toBe(true);
     });
 
-    xit('should post message', function () {
+    it('should init new Slacker', function () {
+      var slacker = Slacker.init(null);
+      expect(typeof slacker == 'object').toBe(true);
+    });
 
-    })
+    it('should post message', function () {
+      var success = function () {};
+      var error = function () {};
+      Slacker.postMessage(success, error, 'message test');
+      expect(execSpy).toHaveBeenCalledWith(
+        success,
+        error,
+        'Slacker',
+        'postMessage',
+        ['message test']
+      );
+    });
+
+    it('post message with empty callbacks should create default functions', function () {
+      Slacker.postMessage(null, null, 'message test');
+      expect(execSpy).toHaveBeenCalledWith(
+        jasmine.any(Function),
+        jasmine.any(Function),
+        'Slacker',
+        'postMessage',
+        ['message test']
+      );
+    });
   });
 });
