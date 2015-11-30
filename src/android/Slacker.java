9bead221e623589592cab2d4aaa5a6ce79e6bbcb
Frederick Jansen <frederick.jansen@gmail.com>
2015-11-30 11:26:35 -0500

    [SKCP-86] Store OAuth token on Android

--- src/android/Slacker.java
+++ src/android/Slacker.java
@@ -1,5 +1,7 @@
 package edu.cscie71.imm.slacker.plugin;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
@@ -12,6 +14,7 @@ public class Slacker extends CordovaPlugin {
     private ISlackerClient slackerClient = new SlackerClient();
     private String token = "xoxp-10020492535-10036686290-14227963249-1cb545e1ae";
     private String immTestChannel = "C0F6U0R5E";
+    private static final String PREFS = "Slacker";
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -28,5 +31,19 @@ public class Slacker extends CordovaPlugin {
             callbackContext.success(message);
         else
             callbackContext.error("Error posting to the Slack channel.");
+    }
+
+    private void storeOAuthToken(String token) {
+        Context context = cordova.getActivity();
+    	SharedPreferences.Editor editor = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE).edit();
+ 		editor.putString("token", token);
+ 		editor.apply();
+    }
+
+    private String getOAuthToken() {
+        Context context = cordova.getActivity();
+    	SharedPreferences prefs = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+        // Return null as default value
+    	return prefs.getString("token", null);
     }
 }
